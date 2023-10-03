82506624de65dbc0aff11f67de0935a6aa704857
PThorpe92 <preston@unlockedlabs.org>
2023-10-03 16:01:00 -0400

    fix: controller name

--- middleware/app/Models/ConsumerPlatform.php
+++ middleware/app/Models/ConsumerPlatform.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ConsumerPlatform extends Model
 {
@@ -18,6 +19,10 @@ class ConsumerPlatform extends Model
 
     protected $hidden = [
         'api_key',
+    ];
+
+    protected $casts = [
+        'type' => ConsumerPlatformType::class
     ];
 
     public function platformConnections(): HasMany
