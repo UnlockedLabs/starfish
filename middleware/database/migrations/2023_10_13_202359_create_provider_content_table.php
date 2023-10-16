9c78d6d6e7d7ce34d81e0af08a4f610dbbfa8511
PThorpe92 <preston@unlockedlabs.org>
2023-10-16 17:36:05 -0400

    fix: add student mapping migration

--- middleware/database/migrations/2023_10_13_202359_create_provider_content_table.php
+++ middleware/database/migrations/2023_10_13_202359_create_provider_content_table.php
@@ -10,8 +10,8 @@ class CreateProviderContentTables extends Migration
     {
         Schema::create('provider_content', function (Blueprint $table) {
             $table->id();
-            $table->integer('content_id');
-            $table->integer('external_resource_id');
+            $table->string('content_id');
+            $table->string('external_resource_id');
             $table->string('type');
             $table->foreign('provider_id')->constrained();
             $table->timestamps();
